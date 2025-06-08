794d72fd4b3fecd45fa000afea462abd0d5a993a
Pradthelegend <pradeep.studies06@gmail.com>
2025-06-08 10:27:02 -0400

    Made changes in all the files added a consturctor in Questions class

--- src/QuestionService.java
+++ src/QuestionService.java
@@ -1,2 +1,19 @@
 public class QuestionService {
+
+    Question[] questions = new Question[5]; // We are creating an array that can hold five references to Question object.
+
+    public QuestionService(){
+
+        questions[0] = new Question(1,"Chin Chin age","1","2","3","4","2");
+        questions[1] = new Question(1,"Chin Chin age","1","2","3","4","2");
+        questions[2] = new Question(1,"Chin Chin age","1","2","3","4","2");
+        questions[3] = new Question(1,"Chin Chin age","1","2","3","4","2");
+        questions[3] = new Question(1,"Chin Chin age","1","2","3","4","2");
+
+
+    }
+    public void displayQuestions(){
+
+        System.out.println(questions[0]);
+    }
 }
